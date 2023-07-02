1c40b875eb1196b623e39bb3935917837ee21181
Tony Leng <lengtk@gmail.com>
2023-07-02 22:16:33 +0900

    defined the color in SketchPicker with useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
-import Callback from "./callback.js";
-import "./styles.css";
+import { useState } from "react";
+import {SketchPicker} from "react-color";
+import "./App.css";
 
 function App() {
+
+  const [currentColor, setCurrentColor] = useState("#ff6")
+
   return (
     <div className="App">
       <h1>React Color Picker</h1>
+
+      <SketchPicker
+      color={currentColor}
+      />
     </div>
   );
 }
