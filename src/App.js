5a5e3bd17e555fb023d766ed6dbf0470098d8949
Tony Leng <lengtk@gmail.com>
2023-07-02 23:02:53 +0900

    added h2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {SketchPicker} from "react-color";
 import "./App.css";
 
 function App() {
-
   const [currentColor, setCurrentColor] = useState("#ff6")
   const handleOnChange = (color) => {
     setCurrentColor(color.hex)
@@ -12,13 +11,12 @@ function App() {
   const appStyle = {
     backgroundColor: currentColor,
     height: "100vh",
-    textAlign: "center",
   }
 
   return (
     <div className="App" style={appStyle}>
       <h1>React Color Picker</h1>
-      <h2>does it update?</h2>
+      <h2>Click on palette to choose color</h2>
 
       <SketchPicker
       color={currentColor}
