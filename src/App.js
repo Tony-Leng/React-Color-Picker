52d7d38640e62d515b8abf709e73bfb06b1ec618
Tony Leng <lengtk@gmail.com>
2023-07-02 22:34:18 +0900

    added currentColor to the backgroundColor

--- src/App.js
+++ src/App.js
@@ -5,14 +5,27 @@ import "./App.css";
 function App() {
 
   const [currentColor, setCurrentColor] = useState("#ff6")
+  const handleOnChange = (color) => {
+    setCurrentColor(color.hex)
+  }
+
+  const appStyle = {
+    backgroundColor: currentColor,
+    height: "100vh",
+    textAlign: "center",
+  }
 
   return (
-    <div className="App">
+    <div className="App" style={appStyle}>
       <h1>React Color Picker</h1>
+      <h2>does it update?</h2>
 
       <SketchPicker
       color={currentColor}
+      onChangeComplete={handleOnChange}
       />
+
+
     </div>
   );
 }
